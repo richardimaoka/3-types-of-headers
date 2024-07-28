1c444a159dae8caba09fe3aa742de66fda3e8411
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-07-28 14:57:19 +0900

    search box focus animation

--- next/app/header2/Search.tsx
+++ next/app/header2/Search.tsx
@@ -1,15 +1,26 @@
 "use client";
 
+import { useState } from "react";
 import styles from "./Search.module.css";
 import { SearchIcon } from "./SearchIcon";
 
 interface Props {}
 
 export function Search(props: Props) {
+  const [focus, setFocus] = useState(false);
+
   return (
-    <div className={styles.component}>
+    <div className={styles.component + (focus ? " " + styles.focus : "")}>
       <SearchIcon />
-      <input className={styles.input} placeholder="search in document" />
+      <input
+        className={styles.input}
+        placeholder="search in document"
+        onFocus={() => {
+          console.log("focus");
+          setFocus(true);
+        }}
+        onBlur={() => setFocus(false)}
+      />
     </div>
   );
 }
